569e9ef642c54d9faac8b67053f2733ee75c8c24
Van Nguyen <van.nguyen2@anz.com>
2023-08-09 17:18:05 +1000

    crunchyroll login

--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/crunchy-labs/crunchyroll-go/v3"
+)
+
 func main() {
+	username := os.Getenv("crunchyUsername")
+	password := os.Getenv("crunchyUsername")
+	client := http.DefaultClient
+
+	_, err := crunchyroll.LoginWithCredentials(username, password, crunchyroll.US, client)
+	if err != nil {
+		fmt.Println("Unsuccessfully logged in", err.Error())
+	}
 }
